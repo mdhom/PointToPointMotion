73899429b25c2f7d960145d3ad353897e5311f78
Max Dhom <maxdhom@outlook.de>
2020-04-19 21:14:07 +0200

    Added JointMotionProfile

---
+++ Point2Point/Calculation/JointMotionProfile/JointMotionProfile.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Point2Point.Calculation.JointMotionProfile
+{
+    public class JointMotionProfile
+    {
+        public List<MotionProfileSegment> Segments { get; } = new List<MotionProfileSegment>();
+
+        public void Append(MotionProfileSegment segment, params MotionProfileSegment[] segments)
+            => Append(new List<MotionProfileSegment>() { segment }.Concat(segments));
+
+        public void Append(IEnumerable<MotionProfileSegment> segments)
+        {
+            foreach (var segment in segments)
+            {
+                segment.Start = Segments.LastOrDefault()?.End ?? 0;
+                Segments.Add(segment);
+            }
+
+            // UpdateProfile();
+        }
+
+        public IEnumerable<MotionProfileSegment> GetEffectiveSegments()
+        {
+            var minVelocityPoints = Segments
+                .SelectMany(s => new[] { new SegmentPoint(s.Start, s.MaximumVelocity), new SegmentPoint(s.End, s.MaximumVelocity) })
+                .OrderBy(sp => sp.Distance)
+                .GroupBy(sp => sp.Distance)
+                .Select(group => new SegmentPoint(group.Key, group.Min(p => p.Velocity)))
+                .ToList();
+
+            var effectiveSegments = new List<MotionProfileSegment>();
+            for (int i = 0; i < minVelocityPoints.Count - 1; i++)
+            {
+                var from = minVelocityPoints[i];
+                var to = minVelocityPoints[i + 1];
+                effectiveSegments.Add(new MotionProfileSegment(from.Distance, to.Distance - from.Distance, from.Velocity));
+            }
+
+            return effectiveSegments;
+        }
+
+        private void UpdateProfile()
+        {
+            var effectiveSegments = GetEffectiveSegments();
+        }
+
+        struct SegmentPoint
+        {
+            public double Distance { get; set; }
+            public double Velocity { get; set; }
+
+            public SegmentPoint(double distance, double velocity)
+            {
+                Distance = distance;
+                Velocity = velocity;
+            }
+        }
+    }
+}
---
+++ Point2Point/Calculation/JointMotionProfile/MotionProfileSegment.cs
@@ -0,0 +1,20 @@
+﻿namespace Point2Point.Calculation.JointMotionProfile
+{
+    public class MotionProfileSegment
+    {
+        public double Start { get; set; }
+        public double Length { get; }
+        public double MaximumVelocity { get; }
+
+        public double End => Start + Length;
+
+        public MotionProfileSegment(double start, double length, double maximumVelocity)
+        {
+            Start = start;
+            Length = length;
+            MaximumVelocity = maximumVelocity;
+        }
+
+        public bool Contains(double distance) => Start <= distance && End > distance;
+    }
+}
