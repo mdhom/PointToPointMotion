c66e485afedd54801b7dae3237d68d594a8750fa
Max Dhom <maxdhom@outlook.de>
2020-04-19 11:17:29 +0200

    Initial commit

---
+++ Point2Point/Calculator.cs
@@ -0,0 +1,272 @@
+﻿using System;
+
+namespace Point2Point
+{
+#pragma warning disable IDE1006 // Naming Styles
+    public class Calculator
+    {
+        private readonly double a_max;
+        private readonly double j_max;
+
+        public double t1 { get; }
+        public double t2 { get; }
+        public double t3 { get; }
+        public double t4 { get; }
+        public double t5 { get; }
+        public double t6 { get; }
+        public double t7 { get; }
+        public int TrajectoryInstanceCase { get; }
+
+        public Calculator(double s, double jMax, double aMax, double vMax)
+        {
+            a_max = aMax;
+            j_max = jMax;
+
+            TrajectoryInstanceCase = GetTrajectoryInstance(s, vMax);
+
+            CalculateTimes(s, vMax, TrajectoryInstanceCase, out var tj, out var ta, out var tv);
+
+            t1 = tj;
+            t2 = ta;
+            t3 = ta + tj;
+            t4 = tv;
+            t5 = tv + tj;
+            t6 = tv + ta;
+            t7 = tv + tj + ta;
+        }
+
+        public void GetStatus(double t, out double j, out double a, out double v, out double s)
+        {
+            if (t <= t1)
+            {
+                GetStatus1(t, out j, out a, out v, out s);
+            }
+            else
+            {
+                GetStatus1(t1, out var j1, out var a1, out var v1, out var s1);
+
+                if (t <= t2)
+                {
+                    GetStatus2(t, a1, v1, s1, out j, out a, out v, out s);
+                }
+                else
+                {
+                    GetStatus2(t2, a1, v1, s1, out var j2, out var a2, out var v2, out var s2);
+
+                    if (t <= t3)
+                    {
+                        GetStatus3(t, a2, v2, s2, out j, out a, out v, out s);
+                    }
+                    else
+                    {
+                        GetStatus3(t3, a2, v2, s2, out var j3, out var a3, out var v3, out var s3);
+
+                        if (t <= t4)
+                        {
+                            GetStatus4(t, v3, s3, out j, out a, out v, out s);
+                        }
+                        else
+                        {
+                            GetStatus4(t4, v3, s3, out var j4, out var a4, out var v4, out var s4);
+
+                            if (t <= t5)
+                            {
+                                GetStatus5(t, v4, s4, out j, out a, out v, out s);
+                            }
+                            else
+                            {
+                                GetStatus5(t5, v4, s4, out var j5, out var a5, out var v5, out var s5);
+
+                                if (t <= t6)
+                                {
+                                    GetStatus6(t, a5, v5, s5, out j, out a, out v, out s);
+                                }
+                                else
+                                {
+                                    GetStatus6(t6, a5, v5, s5, out var j6, out var a6, out var v6, out var s6);
+
+                                    if (t <= t7)
+                                    {
+                                        GetStatus7(t, a6, v6, s6, out j, out a, out v, out s);
+                                    }
+                                    else
+                                    {
+                                        throw new ArgumentOutOfRangeException($"Given time t must be lower than t7 which is {t7:N2} at the moment");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void GetStatus1(double t, out double j, out double a, out double v, out double s)
+        {
+            j = j_max;
+            a = j_max * t;
+            v = 0.5 * j_max * t * t;
+            s = j_max / 6 * t * t * t;
+        }
+
+        private void GetStatus2(double t, double a1, double v1, double s1, out double j, out double a, out double v, out double s)
+        {
+            j = 0;
+            a = a1;
+            v = v1 + a1 * (t - t1);
+            s = s1 + v1 * (t - t1) + 0.5 * a1 * (t - t1) * (t - t1);
+        }
+
+        private void GetStatus3(double t, double a2, double v2, double s2, out double j, out double a, out double v, out double s)
+        {
+            var tPhase = t - t2;
+            var tPhase2 = tPhase * tPhase;
+            var tPhase3 = tPhase2 * tPhase;
+            j = -j_max;
+            a = a2 - j_max * tPhase;
+            v = v2 + a2 * tPhase + 0.5 * -j_max * tPhase2;
+            s = s2 + v2 * tPhase + 0.5 * a2 * tPhase2 + (-j_max / 6) * tPhase3;
+        }
+
+        private void GetStatus4(double t, double v3, double s3, out double j, out double a, out double v, out double s)
+        {
+            var tPhase = t - t3;
+            j = 0;
+            a = 0;
+            v = v3;
+            s = s3 + v3 * tPhase;
+        }
+
+        private void GetStatus5(double t, double v4, double s4, out double j, out double a, out double v, out double s)
+        {
+            var tPhase = t - t4;
+            var tPhase2 = tPhase * tPhase;
+            var tPhase3 = tPhase2 * tPhase;
+            j = -j_max;
+            a = -j_max * tPhase;
+            v = v4 + 0.5 * -j_max * tPhase2;
+            s = s4 + v4 * tPhase + (-j_max / 6) * tPhase3;
+        }
+
+        private void GetStatus6(double t, double a5, double v5, double s5, out double j, out double a, out double v, out double s)
+        {
+            var tPhase = t - t5;
+            var tPhase2 = tPhase * tPhase;
+            j = 0;
+            a = a5;
+            v = v5 - a_max * tPhase;
+            s = s5 + v5 * tPhase + 0.5 * a5 * tPhase2;
+        }
+
+        private void GetStatus7(double t, double a6, double v6, double s6, out double j, out double a, out double v, out double s)
+        {
+            var tPhase = t - t6;
+            var tPhase2 = tPhase * tPhase;
+            var tPhase3 = tPhase2 * tPhase;
+            j = j_max;
+            a = a6 + j_max * tPhase;
+            v = v6 + a6 * tPhase + 0.5 * j_max * tPhase2;
+            s = s6 + v6 * tPhase + 0.5 * a6 * tPhase2 + (j_max / 6) * tPhase3;
+        }
+
+        private int GetTrajectoryInstance(double s, double vMax)
+        {
+            // Case 1: a_max reached, v_max reached and constant for a time                             
+            //          (s = 100,   j = 2000, a = 500,  vMax = 120)
+            // Case 2: a_max not reached, v_max not reached                                             
+            //          (s = 15000, j = 2000, a = 5500, vMax = 20500)
+            // Case 3: a_max not reached, v_max reached and constant for a time                         
+            //          (s = 15000, j = 2000, a = 5500, vMax = 2500)
+            // Case 4: a_max reached, v_max not reached                                                 
+            //          (s = 57,    j = 2000, a = 500,  vMax = 120)
+            // Case 5: a_max reached and constant for a time, v_max reached and constant for a time     
+            //          (s = 15000, j = 2000, a = 500,  vMax = 2500)
+            // Case 6: a_max reached and constant for a time, v_max not reached                         
+            //          (s = 15000, j = 2000, a = 500,  vMax = 20500)
+
+            var v_a = a_max * a_max / j_max;
+            var s_a = (2 * a_max * a_max * a_max) / (j_max * j_max);
+            double s_v;
+            if (vMax * j_max < a_max * a_max)
+            {
+                s_v = vMax * 2 * Math.Sqrt(vMax / j_max);
+            }
+            else
+            {
+                s_v = vMax * (vMax / a_max + a_max / j_max);
+            }
+
+            if (vMax < v_a && s > s_a)
+            {
+                return 1;
+            }
+            else if (vMax > v_a && s < s_a)
+            {
+                return 2;
+            }
+            else if (vMax < v_a && s < s_a && s > s_v)
+            {
+                return 3;
+            }
+            else if (vMax < v_a && s < s_a && s < s_v)
+            {
+                return 4;
+            }
+            else if (vMax > v_a && s > s_a && s > s_v)
+            {
+                return 5;
+            }
+            else if (vMax > v_a && s > s_a && s < s_v)
+            {
+                return 6;
+            }
+            else
+            {
+                throw new InvalidOperationException("Undetected case");
+            }
+        }
+
+        private void CalculateTimes(double s, double vMax, int trajectoryInstance, out double tj, out double ta, out double tv)
+        {
+            switch (trajectoryInstance)
+            {
+                case 1:
+                case 3:
+                    tj = Math.Sqrt(vMax / j_max);
+                    ta = tj;
+                    tv = s / vMax;
+                    return;
+                case 2:
+                case 4:
+                    tj = ThirdRoot(s / (2 * j_max));
+                    ta = tj;
+                    tv = 2 * tj;
+                    return;
+                case 5:
+                    tj = a_max / j_max;
+                    ta = vMax / a_max;
+                    tv = s / vMax;
+                    return;
+                case 6:
+                    tj = a_max / j_max;
+                    ta = 0.5 * (Math.Sqrt((4 * s * j_max * j_max + a_max * a_max * a_max) / (a_max * j_max * j_max)) - a_max / j_max);
+                    tv = ta + tj;
+                    return;
+                default:
+                    throw new ArgumentOutOfRangeException($"TrajectoryInstance must be between 1 and 6");
+            }
+        }
+
+        private static double ThirdRoot(double x)
+        {
+            return Math.Pow(x, 1.0 / 3.0);
+        }
+
+        public double GetTotalDistance()
+        {
+            GetStatus(t7, out _, out _, out _, out var s);
+            return s;
+        }
+    }
+#pragma warning restore IDE1006 // Naming Styles
+}
---
+++ Point2Point/MainViewModel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using OxyPlot;
+
+namespace Point2Point
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private double _targetDistance = 15000;
+        public double TargetDistance
+        {
+            get => _targetDistance;
+            set
+            {
+                _targetDistance = value;
+                Update();
+            }
+        }
+
+        private double _velocityMax = 1500;
+        public double VelocityMax
+        {
+            get => _velocityMax;
+            set
+            {
+                _velocityMax = value;
+                Update();
+            }
+        }
+
+        private double _jerkMax = 2000;
+        public double JerkMax
+        {
+            get => _jerkMax;
+            set
+            {
+                _jerkMax = value;
+                Update();
+            }
+        }
+
+        private double _accelerationMax = 500;
+        public double AccelerationMax
+        {
+            get => _accelerationMax;
+            set
+            {
+                _accelerationMax = value;
+                Update();
+            }
+        }
+
+        public double ResultDuration { get; private set; }
+        public double ResultDistance { get; private set; }
+        public int ResultTrajectoryInstanceCase { get; private set; }
+
+        public List<DataPoint> DataJ { get; private set; }
+        public List<DataPoint> DataA { get; private set; }
+        public List<DataPoint> DataV { get; private set; }
+        public List<DataPoint> DataS { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+            Update();
+        }
+
+        private void Update()
+        {
+            DataJ = new List<DataPoint>();
+            DataA = new List<DataPoint>();
+            DataV = new List<DataPoint>();
+            DataS = new List<DataPoint>();
+            ResultDuration = 0;
+            ResultTrajectoryInstanceCase = 0;
+            ResultDistance = 0;
+
+            try
+            {
+                var calc = new Calculator(TargetDistance, JerkMax, AccelerationMax, VelocityMax);
+
+                for (double t = 0; t <= calc.t7; t += 0.001)
+                {
+                    calc.GetStatus(t, out var j, out var a, out var v, out var s);
+                    DataJ.Add(new DataPoint(t, j));
+                    DataA.Add(new DataPoint(t, a));
+                    DataV.Add(new DataPoint(t, v));
+                    DataS.Add(new DataPoint(t, s));
+                }
+
+                ResultDuration = calc.t7;
+                ResultTrajectoryInstanceCase = calc.TrajectoryInstanceCase;
+                ResultDistance = calc.GetTotalDistance();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}\r\n{ex.StackTrace}");
+            }
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DataJ)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DataA)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DataV)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DataS)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ResultDuration)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ResultTrajectoryInstanceCase)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ResultDistance)));
+        }
+    }
+}
