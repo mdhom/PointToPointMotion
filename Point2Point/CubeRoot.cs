efaa88dcd74bee4ade89178e848a931b1c3c29db
Max Dhom <maxdhom@outlook.de>
2020-04-22 08:37:45 +0200

    CubeRoot._third readonly

--- Point2Point/CubeRoot.cs
+++ Point2Point/CubeRoot.cs
@@ -8,7 +8,8 @@ namespace System
 {
     public static class CubeRoot
     {
-        private static double _third = 1.0 / 3;
+        private static readonly double _third = 1.0 / 3;
+
         public static double Get(double x)
         {
             return Math.Pow(x, _third);
